a5d0e13d68caf2795eec44b3e83974add4af84be
Sohambasuri-2002 <sohambasuri2015@gmail.com>
2023-10-09 23:11:37 +0530

    change in profile photo

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if($_SESSION['$message'])
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>STUDENT PORTAL</title>
+    <title>EduTECH</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     
@@ -46,7 +46,7 @@ if($_SESSION['$message'])
 
         <div class="section1">
 
-            <img class="main_logo" src="image/edu2.svg">
+            <img class="main_logo" src="image/edu.jpg">
         </div>
 
 
